9dd2d8bc7dba3217402be28e07fbdcbf953695a6
cdibilio <cdibilio@princeton.edu>
2019-01-29 20:43:22 -0500

    ad testing

--- Complete/Controller/taskVC.swift
+++ Complete/Controller/taskVC.swift
@@ -782,9 +782,11 @@ class taskVC: UIViewController, UITableViewDataSource, UITableViewDelegate, GADB
     // Helper function for banner ads
     func loadBannerView() {
         // Test ID now
-        bannerAd.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerAd.adUnitID = "ca-app-pub-5383101165774401/3144244729"
         bannerAd.rootViewController = self
-        bannerAd.load(GADRequest())
+        let request = GADRequest()
+        request.testDevices = [ "167eaa51497ced63b2cf31912f7d2cab" ]
+        bannerAd.load(request)
     }
     
     
